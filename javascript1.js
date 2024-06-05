25d8d42e7d325a7ee9973a04c1f828d3ef75ca44
Najwan <144207666+Najwan786@users.noreply.github.com>
2024-06-05 10:37:03 +0400

    Fifa 23 website

---
+++ javascript1.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById('splash').style.display = 'none';
+        document.getElementById('main-content1').style.display = 'block';
+    }, 3000); // 3000ms = 3 seconds
+});
---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector('button').addEventListener('click', function() {
+    alert('Learn more about FIFA 23 World Cup!');
+});
